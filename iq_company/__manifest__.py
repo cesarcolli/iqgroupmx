1c737d4e2758506701692a66348153961f59ff26
cesar.colli <cesar.colli@bsdenterprise.com>
2018-08-10 09:07:12 -0500

    [NEW] company form customization, REQ ID [VENTAS_010]

---
+++ iq_company/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'IQ company',
+    'version': '1.1',
+    'category': 'IQ',
+    'description': """
+IQ company
+====================================================
+    """,
+    'depends': ['base'],
+    'data': [
+        'views/iq_company_view_form.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+}
