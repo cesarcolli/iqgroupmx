afe6054bccc0922f8746a6d804c77f6d7d0b022b
cesar.colli <cesar.colli@bsdenterprise.com>
2018-08-27 09:43:19 -0500

    [NEW] Module for general partner customizations Requirements: [VENTAS_009, VENTAS_011]

---
+++ iq_partner/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "author": "Cesar Colli, "
+              "Odoo Community Association (OCA)",
+    "name": "IQ Clientes",
+    "version": "1.0.0",
+    "category": "Generic Modules/Base",
+    "depends": [
+        'base',
+    ],
+    "summary": "Customización de Clientes",
+    "data": [
+        'data/partner_sequence.xml',
+        'views/partner_view.xml',
+    ],
+    "installable": True
+}
