599ade1110ce280e595d6aed6705ea3bc7e9258a
cesar.colli <cesar.colli@bsdenterprise.com>
2018-09-03 21:38:24 -0500

    [CHANGES] New field supplier ref added to partner form

--- iq_partner/models/partner.py
+++ iq_partner/models/partner.py
@@ -19,5 +19,43 @@ class ResPartner(models.Model):
     def _default_interest(self):
         return self.env['partner.interest'].browse(self._context.get('x_interest_id'))
 
+    x_customer_ref = fields.Char(string='Referencia Cliente', index=True)
+    x_supplier_ref = fields.Char(string='Referencia Proveedor', index=True)
+
     x_interest_id = fields.Many2many('partner.interest', column1='partner_id',
                                      column2='x_interest_id', string='Categorías de interés', default=_default_interest)
+
+    @api.multi
+    def _get_next_customer_ref(self, vals=None):
+        return self.env['ir.sequence'].next_by_code('res.partner.customer')
+
+    @api.multi
+    def _get_next_supplier_ref(self, vals=None):
+        return self.env['ir.sequence'].next_by_code('res.partner.supplier')
+
+    @api.model
+    def create(self, vals):
+        if vals.get("customer") and not vals.get('x_customer_ref'):
+            vals['x_customer_ref'] = self._get_next_customer_ref(vals=vals)
+        if vals.get("supplier") and not vals.get('x_supplier_ref'):
+            vals['x_supplier_ref'] = self._get_next_supplier_ref(vals=vals)
+        return super(ResPartner, self).create(vals)
+
+    @api.multi
+    def copy(self, default=None):
+        default = default or {}
+        if default['x_customer_ref']:
+            default['x_customer_ref'] = self._get_next_customer_ref()
+        if default['x_supplier_ref']:
+            default['x_supplier_ref'] = self._get_next_supplier_ref()
+        return super(ResPartner, self).copy(default)
+
+    @api.multi
+    def write(self, vals):
+        for partner in self:
+            if not vals.get('x_customer_ref') and vals.get("customer") and not partner.x_customer_ref:
+                vals['x_customer_ref'] = self._get_next_customer_ref(vals=vals)
+            if not vals.get('x_supplier_ref') and  vals.get("supplier") and not partner.x_supplier_ref:
+                vals['x_supplier_ref'] = self._get_next_supplier_ref(vals=vals)
+            super(ResPartner, partner).write(vals)
+        return True
