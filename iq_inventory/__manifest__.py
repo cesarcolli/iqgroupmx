8d1571e1bee1a91c7adeddb5dc502fccada1da42
cesar.colli <cesar.colli@bsdenterprise.com>
2018-09-08 19:09:31 -0500

    [NEW] Dimensions fields Lenght, Width and Heigh added

--- iq_inventory/__manifest__.py
+++ iq_inventory/__manifest__.py
@@ -13,13 +13,14 @@
 
     # Categories can be used to filter modules in modules listing
     'category': 'IQ',
-    'version': '1.0',
+    'version': '1.3',
 
     # any module necessary for this one to work correctly
     'depends': ['sale_stock'],
 
     # always loaded
     'data': [
+        'views/product_views.xml',
         'reports/iq_stock_picking_main.xml',
         'reports/iq_stock_picking_move.xml',
         'reports/iq_stock_picking_delivery.xml',
